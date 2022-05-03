a1a2592dae1b9a4191677ddbda70e4d1ae929040
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-05-03 18:35:00 +0300

    Feat: add menu invokation, news pagination

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import type { NewsFormat } from './types';
+import { getNewsData } from './NewsHandler';
+import { Bot } from 'grammy';
+import { Menu } from '@grammyjs/menu';
+
+const BOT_TOKEN = '5360109659:AAGwqfPOimT-bAHiFnFDhXhd5kFR-rE_L20';
+
+const bot = new Bot(BOT_TOKEN);
+let news: NewsFormat[] = [];
+let lastShownNewsIdx = 0;
+
+const menu = new Menu('my-menu-identifier')
+    .text('List all news titles', async (ctx) => {
+        const titles: string[] = [];
+        for (const { title } of news) {
+            titles.push(title);
+        }
+        await ctx.reply(titles.map((title, idx) => `<b>${idx + 1}:</b> ${title}`).join('\n'), { parse_mode: 'HTML' });
+    }).row().text('Load next 5 news', async (ctx) => {
+        for (const { creationTime, title, url } of news.slice(lastShownNewsIdx,lastShownNewsIdx + 5)) {
+            const formattedNews = `<b>Title:</b> ${title}\n<b>PostTime:</b> ${creationTime}\n<b>Link:</b> ${url}`;
+            ctx.reply(formattedNews, { parse_mode: 'HTML' });
+        }
+        lastShownNewsIdx += 5;
+        await ctx.reply('You have options to proceed', { reply_markup: menu });
+    });   
+
+bot.use(menu);
+bot.command('getnews', async (ctx) => {
+    news = (await getNewsData()) || [];
+    await ctx.reply(`I received ${news.length} news`);
+    await ctx.reply('You have options to proceed', { reply_markup: menu });
+});
+bot.command('menu', async (ctx) => {
+    await ctx.reply('Here is your menu', { reply_markup: menu });
+});
+bot.start();
+bot.catch((error) => {
+    console.dir(error);
+});
