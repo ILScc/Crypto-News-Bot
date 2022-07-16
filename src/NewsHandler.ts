bfa9d993cb68da8da21fd3f8894568ea753a6df1
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-07-16 21:53:07 +0300

    Store bot key

--- src/NewsHandler.ts
+++ src/NewsHandler.ts
@@ -2,10 +2,10 @@ import { fetch } from  'undici' ;
 import { ResponseType } from './types';
 import { formatDate } from './helpers';
 
-const API_KEY = '088535a8670f925a47853a2ec672d91f9425240c0c3d2883b5cd40d57b9ef8d6' ;
+
 
 async function fetchNews() {
-    const res = await fetch(`https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=${API_KEY}`);
+    const res = await fetch(`https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=${process.env.BOT_TOKEN}`);
     const news = await res.json();
     return news;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import type { NewsFormat } from './types';
 import { getNewsData } from './NewsHandler';
 import { Bot } from 'grammy';
 import { Menu } from '@grammyjs/menu';
-import { BOT_API_KEY } from '../secrets';
+import * as dotenv from 'dotenv';
 
-const bot = new Bot(BOT_API_KEY);
+dotenv.config();
+console.log(process.env); 
+
+const bot = new Bot(process.env.BOT_TOKEN!);
 let news: NewsFormat[] = [];
 let lastShownNewsIdx = 0;
 bot.api.setMyCommands([
     { command: 'news', description: 'Load news' },
     { command: 'help', description: 'Show help text' },
     { command: 'settings', description: 'Open settings' },
-    { command: 'find', description: 'Find article by name' }, // TODO: реализовать добавление
+    { command: 'find', description: 'Find article by name' },
 ]);
 const menu = new Menu('mainMenu')
     .text('List all news titles', async (ctx) => {
@@ -19,25 +22,29 @@ const menu = new Menu('mainMenu')
         for (const { title } of news) {
             titles.push(title);
         }
-        await ctx.reply(titles.map((title, idx) => `<b>${idx + 1}:</b> ${title}`).join('\n'), { parse_mode: 'HTML' });
-    }).row().text('Load next 5 news', async (ctx) => {
-        for (const { creationTime, title, url } of news.slice(lastShownNewsIdx,lastShownNewsIdx + 5)) {
+        await ctx.reply(
+            titles.map((title, idx) => `<b>${idx + 1}:</b> ${title}`).join('\n'),
+            { parse_mode: 'HTML' }
+        );
+    })
+    .row()
+    .text('Load next 5 news', async (ctx) => {
+        for (const { creationTime, title, url } of news.slice(
+            lastShownNewsIdx,
+            lastShownNewsIdx + 5
+        )) {
             const formattedNews = `<b>Title:</b> ${title}\n<b>PostTime:</b> ${creationTime}\n<b>Link:</b> ${url}`;
             ctx.reply(formattedNews, { parse_mode: 'HTML' });
         }
         lastShownNewsIdx += 5;
         await ctx.reply('You have options to proceed', { reply_markup: menu });
-    });   
-
+    });
 bot.use(menu);
 bot.command('news', async (ctx) => {
     news = (await getNewsData()) || [];
     await ctx.reply(`I received ${news.length} news`);
     await ctx.reply('You have options to proceed', { reply_markup: menu });
 });
-// bot.command('menu', async (ctx) => {
-//     await ctx.reply('Here is your menu', { reply_markup: menu });
-// });
 bot.start();
 bot.catch((error) => {
     console.dir(error);
